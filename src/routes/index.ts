ad8624533de3c1a2ff602492f023daae81f46a2f
Tomi Helin <tomi.helin@student.lut.fi>
2025-01-05 20:45:13 +0200

    Task 2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,57 +3,78 @@ import bcrypt from "bcrypt"
 import { compile } from "morgan"
 import {User} from "../models/User"
 import jwt, {JwtPayload} from "jsonwebtoken"
+import {body, validationResult} from 'express-validator'
 
 const router: Router = Router()
 
-router.post("/api/user/register", async (req: Request, res: Response) => {
-    const {email, password, username, isAdmin} = req.body;
+router.post("/api/user/register", 
+    body('email').isEmail().withMessage('Invalid email').normalizeEmail(),
+    body('username').trim().isLength({min: 3, max:25}).withMessage('Username must be between 3 and 25 characters').escape(),
+    body('password').isStrongPassword({minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1}).withMessage('Password must be at least 8 characters long, contain uppercase and lowercase letters, a number, and a special character'),
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array()});
+            return
+        }
 
-    try {
-        // Check if the email is already taken
-        const existingUser = await User.findOne({email});
-        if (existingUser) {
-            res.status(403).json({ message: 'Email already in use.' });
+        const {email, password, username, isAdmin} = req.body;
+
+        try {
+            // Check if the email is already taken
+            const existingUser = await User.findOne({email});
+            if (existingUser) {
+                res.status(403).json({ message: 'Email already in use.' });
+                return 
+            }
+
+            const salt: string = bcrypt.genSaltSync(10);
+            const hash: string = bcrypt.hashSync(password, salt);
+
+            const newUser = new User({ email, password: hash, username, isAdmin });
+            await newUser.save();
+
+            res.status(200).json(newUser);
+            return
+        } catch (error) {
+            res.status(500).json({message: 'Internal server error.'});
+            return
+        }
+})
+
+router.post("/api/user/login", 
+    body('email').isEmail().withMessage('Invalid email').normalizeEmail(),
+    body('password').notEmpty().withMessage('Password is required'),
+    async (req: Request, res: Response) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array()});
+            return
+        }
+
+        const {email, password} = req.body;
+
+        const loginUser = await User.findOne({email});
+        if (!loginUser) {
+            res.status(404).json({ message: 'Login failed.' });
             return 
         }
 
-        const salt: string = bcrypt.genSaltSync(10);
-        const hash: string = bcrypt.hashSync(password, salt);
+        if (!bcrypt.compareSync(password, loginUser.password)) {
+            res.status(401).json({ message: 'Login failed.' });
+            return
+        }
 
-        const newUser = new User({ email, password: hash, username, isAdmin });
-        await newUser.save();
+        const jwtPayload: JwtPayload = {
+            user: loginUser._id,
+            username: loginUser.username,
+            isAdmin: loginUser.isAdmin
+        }
+        const token: string = jwt.sign(jwtPayload, process.env.SECRET as string, { expiresIn: "2m"})
 
-        res.status(200).json(newUser);
+        res.status(200).json({success: true, token})  
         return
-    } catch (error) {
-        res.status(500).json({message: 'Internal server error.'});
-        return
-    }
-})
-
-router.post("/api/user/login", async (req: Request, res: Response) => {
-    const {email, password} = req.body;
-
-    const loginUser = await User.findOne({email});
-    if (!loginUser) {
-        res.status(404).json({ message: 'Login failed.' });
-        return 
-    }
-
-    if (!bcrypt.compareSync(password, loginUser.password)) {
-        res.status(401).json({ message: 'Login failed.' });
-        return
-    }
-
-    const jwtPayload: JwtPayload = {
-        user: loginUser._id,
-        username: loginUser.username,
-        isAdmin: loginUser.isAdmin
-    }
-    const token: string = jwt.sign(jwtPayload, process.env.SECRET as string, { expiresIn: "2m"})
-
-    res.status(200).json({success: true, token})  
-    return
 })
 
 export default router
